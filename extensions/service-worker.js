33fd734b7f94533db2b454c09cf4646337002299
Sachet Ghimire <sachetghimire3@gmail.com>
2026-01-28 14:56:37 +0545

    Added: switch cases for listening to messages from content script, handle checking if the model is loaded.

--- extensions/service-worker.js
+++ extensions/service-worker.js
@@ -1,7 +1,55 @@
-// Minimal MV3 service worker
+//service worekr: message corrdinater between popup and content script./ 
 console.log('Service Worker loaded');
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  // Acknowledge messages so background script is valid
-  sendResponse({ ok: true });
+//track model status
+let modelReady = false;
+
+//listen here for extension installs and updates
+chrome.runtime.onInstalled.addListener((details) => {
+  console.log('extension updated', details.reason);
+
+  if (details.reason === "install"){
+    console.log('extension installed');
+  }
+   else if (details.reason === "update"){
+    console.log('extension updated');
+   }
 });
+
+
+//listen for messages from the popup or the content script
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log('message received', request);
+
+  //handle different types of messages
+
+  switch (request.action0){
+    case "CHECK_MODEL_STATUS":
+      handleCheckModelStatus(request, sender, sendResponse);
+      return true; //keep channel open for async responses
+
+      case "MODEL_LOADED":
+        handleCheckModelLoaded(request, sender, sendResponse);
+        return false;
+
+        case "DETECT_IMAGES":
+          handleDetectImages(request, sender, sendResponse);
+          return true; //ksame as up
+
+          case "UPSCALE_SINGLE_IMAGE":
+            handleUpscaleImage(request, sender, sendResponse);
+            return false; //same
+
+            default:
+              console.log('unknown message', request.action);
+              sendResponse({ error: 'unknown message'});
+              return false;
+  }
+});
+
+
+//handler; check if model is loaded
+function handleCheckModelStatus(request, sender, sendResponse) {
+  console.log('checking model status', modelReady);
+  sendResponse({ modelReady: modelReady });
+}
